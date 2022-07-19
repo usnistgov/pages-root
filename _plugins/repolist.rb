cc65dd566bd78333fe86ea84fa1de842d06b08be
Steve Barber <steve.barber@nist.gov>
2022-07-19 14:52:16 -0400

    Add blacklist capability to repolist jekyll module

--- _plugins/repolist.rb
+++ _plugins/repolist.rb
@@ -5,6 +5,7 @@ module Jekyll
   class RepoListTag < Liquid::Tag
 
     @@repodir = '/srv/web/pages-generated'        # Where all the published repos reside
+    @@blacklist = '/home/ubuntu/repo-blacklist'   # Contains a list of repos NOT to include in the generated repo list
 
     def initialize(tag_name, text, tokens)
       super
@@ -23,8 +24,17 @@ module Jekyll
       end
     end
 
+    def _get_blacklist(filename)
+      if FileTest.file?(filename)
+        return File.readlines(filename)
+      else
+        return []
+      end
+    end
+
     def render(context)
-      repos = Dir.entries(@@repodir).select {|entry| File.directory? File.join(@@repodir, entry) and !(entry == '.' || entry == '..') }
+      blist = _get_blacklist(@@blacklist)
+      repos = Dir.entries(@@repodir).select {|entry| File.directory? File.join(@@repodir, entry) and !(entry == '' || entry == '.' || entry == '..' || blist.include?(entry)) }
       repos = repos.sort
       if repos.empty?
         s = "<p>(No published repos found!)</p>"
